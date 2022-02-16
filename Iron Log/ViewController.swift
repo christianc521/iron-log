fdf12580866cecc86f5cb8ab1552ea4d4482fe6b
Christian Cruz <seseko98@gmail.com>
2022-02-15 20:12:56 -0600

    Initial Commit

---
+++ Iron Log/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Iron Log
+//
+//  Created by Christian Cruz on 2/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
